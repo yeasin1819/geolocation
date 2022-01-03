fb2ee82c83a6d5f6fc3c0b1d28bc36218959a56d
tanimSrabon13 <inamul.ke@gmail.com>
2022-01-03 17:38:41 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Geo Location Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        
+        appBar:
+       AppBar(title: const Text('Geo Location Tracker'),
+       centerTitle: true,
+       ),
+      body: const MyHomePage(),
+      ),
+      
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  
+ Future getLocation()async{
+   try{
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low);
+     setState(() {
+      position =position;
+     });
+   }catch(err){}
+  }
+  @override
+  void initState() {
+   
+    super.initState();
+    getLocation();
+  }
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+
+   
+   return Container(child:
+    Center(child: 
+      Column(
+     mainAxisAlignment: MainAxisAlignment.center,
+     children: [
+     
+     const Icon(Icons.location_on,size: 100,color: Colors.red,),
+     const Text('Get the Location',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+     Text(""),
+     RaisedButton(onPressed: getLocation,
+     child:
+      const Text('get location',
+      style: TextStyle(color: Colors.white),
+      ),
+      color: Colors.blue,),
+    ],
+   ),
+   ),
+   );
+  }
+}
